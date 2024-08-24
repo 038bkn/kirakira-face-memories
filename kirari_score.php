8997f85651fcff43da5ef5b78e8d7c594610c097
038bkn <038bkn@gmail.com>
2024-08-24 20:50:45 +0900

    評価画面でけた

--- kirari_score.php
+++ kirari_score.php
@@ -1,6 +1,7 @@
 <?php
 require './config.php';
 
+// データベース接続
 $conn = new mysqli($servername, $username, $password, $database);
 
 // 接続確認
@@ -10,6 +11,7 @@ if ($conn->connect_error) {
 
 $imageData = null;
 
+// 画像IDが指定されている場合
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
 
@@ -20,6 +22,7 @@ if (isset($_GET['id'])) {
         $stmt->execute();
         $result = $stmt->get_result();
 
+        // 結果が存在する場合
         if ($result->num_rows > 0) {
             $imageData = $result->fetch_assoc();
         }
@@ -60,20 +63,46 @@ $conn->close();
             </div>
         </div>
     </header>
+
     <main>
         <div class="container">
             <?php if ($imageData): ?>
                 <div class="image-preview">
                     <img src="uploads/<?php echo htmlspecialchars($imageData['file_name']); ?>" alt="アップロードされた画像">
                 </div>
-                <p>キラリ☆度: <?php echo htmlspecialchars($imageData['kirari_score']); ?>点</p>
-                <p>評価: <?php echo str_repeat('★', intval($imageData['kirari_score'] / 20)); ?></p>
-                <p>コメント: <?php echo htmlspecialchars($imageData['comments']); ?></p>
+                <div class="sample_box13">
+                    <?php
+                    // 星の数を5個固定
+                    $totalStars = 5;
+                    // キラリ☆度に基づいて埋める星の数を計算
+                    $filledStars = intval($imageData['kirari_score'] / 20);
+                    // 空の星の数を計算
+                    $emptyStars = $totalStars - $filledStars;
+                    ?>
+
+                    <div class="stars-container">
+                        <?php for ($i = 0; $i < $filledStars; $i++): ?>
+                            <span>★</span>
+                        <?php endfor; ?>
+                        <?php for ($i = 0; $i < $emptyStars; $i++): ?>
+                            <span>☆</span>
+                        <?php endfor; ?>
+                    </div>
+                    <p>🌟キラリ☆度 ☞ <?php echo htmlspecialchars($imageData['kirari_score']); ?>キラリ☆彡</p>
+
+                    <p>💬コメント ☞ <?php echo htmlspecialchars($imageData['comments']); ?></p>
+                </div>
+                <div class="subnav-container">
+                    <a href="upload_face.html" class="subnav" id="upload-now">もう一度評価する👀👀</a>
+                    <a href="#" class="subnav">結果をシェア🌷</a>
+                </div>
+
             <?php else: ?>
-                <p>評価する画像がありません。</p>
+                <p>評価する画像がナイヨ～💦💦</p>
             <?php endif; ?>
         </div>
     </main>
+
     <footer>
         <div class="container">
             <p>&copy; 2024 キラリ☆. All rights reserved.</p>
