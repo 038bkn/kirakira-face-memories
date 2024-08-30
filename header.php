faa427fc0cccb43514620831e79a33691f56733a
038bkn <038bkn@gmail.com>
2024-08-30 09:46:26 +0900

    chore: Update header styles and add responsive menu button

--- header.php
+++ header.php
@@ -1,18 +1,22 @@
-<header>
+<header class="header_bg">
     <div class="container">
-        <div class="header-left">
+        <div class="header-left header_contents">
             <img class="logo" src="./images/logo.png" alt="キラめもロゴ">
         </div>
         <div class="header-center">
             <h1><?php echo basename($_SERVER['PHP_SELF']) == 'upload_face.php' ? '自撮りアップロード' : 'ホーム'; ?></h1>
         </div>
-        <div class="header-right">
-            <ul>
-                <li><a href="index.php">ホーム</a></li>
-                <li><a href="#">アルバム</a></li>
-                <li><a href="#">チャレンジ</a></li>
-                <li><a href="#">ランキング</a></li>
-            </ul>
+        <div class="responsive_btn">
+            <div class="menu_line"></div>
+            <div class="menu_line"></div>
+            <div class="menu_line"></div>
         </div>
+        <nav class="header-right header_nav">
+            <ul class="header_nav_lists">
+                <li><a class="nav_link" href="index.php">ホーム</a></li>
+                <li><a class="nav_link" href="#">アルバム</a></li>
+                <li><a class="nav_link" href="#">ランキング</a></li>
+            </ul>
+        </nav>
     </div>
 </header>
--- index.php
+++ index.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="./css/common.css">
     <link rel="stylesheet" href="./css/uploadarea.css">
     <link rel="stylesheet" href="./css/home.css">
+    <link rel="stylesheet" href="./css/header.css">
 </head>
 
 <body>
@@ -55,6 +56,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="./js/script.js"></script>
+    <script src="js/header.js"></script> 
 </body>
 
 </html>
